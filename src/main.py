adfe025b2089020d390956eb5ab7358be7ded000
rEzA <eyvatanreza@gmail.com>
2023-09-17 23:08:14 +0330

    step 1

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple, Union
+
+import pandas as pd
+from src.utils.purchase_details import Purchase
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
